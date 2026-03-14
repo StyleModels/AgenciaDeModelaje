4fbd83721d787766952d507a5876e157ff666e0c
01Mauricio <mauriciosanta1112@gmail.com>
2026-03-14 01:31:22 -0500

    aaa

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* CARGAR MENU GLOBAL */
+document.addEventListener("DOMContentLoaded", function(){
 
 fetch("./menu.html")
 .then(response => response.text())
@@ -6,64 +6,15 @@ fetch("./menu.html")
 
 document.getElementById("menu-container").innerHTML = data;
 
-/* DESPUES DE CARGAR EL MENU */
+/* REINICIAR BOOTSTRAP PARA EL MENU */
 
-const header = document.querySelector(".navbar-stylemodels");
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+collapseElementList.map(function (collapseEl) {
+return new bootstrap.Collapse(collapseEl, {
+toggle: false
+})
+})
 
-let lastScroll = 0;
-let hideTimer;
+})
 
-/* detectar si es pantalla grande */
-
-function isDesktop(){
-    return window.innerWidth > 992;
-}
-
-/* OCULTAR AL BAJAR SCROLL */
-
-window.addEventListener("scroll", function(){
-
-    if(!isDesktop()) return;
-
-    let currentScroll = window.pageYOffset;
-
-    if(currentScroll > lastScroll){
-        header.style.top = "-90px";
-    }else{
-        header.style.top = "0";
-    }
-
-    lastScroll = currentScroll;
-
-    resetTimer();
-
-});
-
-/* OCULTAR SI NO SE USA */
-
-function resetTimer(){
-
-    if(!isDesktop()) return;
-
-    clearTimeout(hideTimer);
-
-    hideTimer = setTimeout(function(){
-        header.style.top = "-90px";
-    },2000);
-
-}
-
-/* MOSTRAR CUANDO EL CURSOR PASA ARRIBA */
-
-document.addEventListener("mousemove", function(e){
-
-    if(!isDesktop()) return;
-
-    if(e.clientY < 80){
-        header.style.top = "0";
-        resetTimer();
-    }
-
-});
-
-});
+})
