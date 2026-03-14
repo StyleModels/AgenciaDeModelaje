c13daafaa31fd01f8e9e9fe0637e341ceddcea3e
01Mauricio <mauriciosanta1112@gmail.com>
2026-03-13 22:24:00 -0500

    Modificacion general de la pagina

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
 console.log("Style Models website loaded");
 
-/* ===== CONTROL MENU AUTO HIDE ===== */
-
 let header = document.querySelector(".header");
+
 let lastScroll = 0;
-let inactivityTimer;
+let hideTimer;
 
-/* ocultar al bajar scroll */
-window.addEventListener("scroll", () => {
+// OCULTAR CUANDO HACES SCROLL HACIA ABAJO
+window.addEventListener("scroll", function(){
 
-let currentScroll = window.pageYOffset;
+    let currentScroll = window.pageYOffset;
 
-if(currentScroll > lastScroll){
-header.classList.add("hide-menu");
-}else{
-header.classList.remove("hide-menu");
-}
+    if(currentScroll > lastScroll){
+        header.classList.add("hide");
+    }else{
+        header.classList.remove("hide");
+    }
 
-lastScroll = currentScroll;
+    lastScroll = currentScroll;
 
-resetTimer();
-
+    resetTimer();
 });
 
-/* mostrar al mover el mouse arriba */
-document.addEventListener("mousemove", (e) => {
 
-if(e.clientY < 80){
-header.classList.remove("hide-menu");
-}
-
-});
-
-/* ocultar por inactividad */
+// OCULTAR SI NO SE USA EN 2 SEGUNDOS
 function resetTimer(){
 
-clearTimeout(inactivityTimer);
+    clearTimeout(hideTimer);
 
-inactivityTimer = setTimeout(() => {
-header.classList.add("hide-menu");
-},3000);
+    hideTimer = setTimeout(function(){
+        header.classList.add("hide");
+    },2000);
 
 }
 
-resetTimer();
+
+// MOSTRAR CUANDO EL CURSOR PASA POR ARRIBA
+header.addEventListener("mouseenter", function(){
+    header.classList.remove("hide");
+});
+
+
+// MOSTRAR SI EL CURSOR ESTA EN LA PARTE SUPERIOR
+document.addEventListener("mousemove", function(e){
+
+    if(e.clientY < 80){
+        header.classList.remove("hide");
+        resetTimer();
+    }
+
+});
